682cd210e535c698dd09f12cf9cbbcd295506347
botlikes <bradwoo8621@yandex.com>
2021-04-21 19:11:34 +0800

    initialize

--- src/main/kotlin/com/imma/model/core/compute/ConstantParameterKits.kt
+++ src/main/kotlin/com/imma/model/core/compute/ConstantParameterKits.kt
@@ -7,11 +7,16 @@ typealias GetFirstValue = (propertyName: String) -> Any?
 class ConstantParameterKits {
 	companion object {
 		/**
+		 * variable can be:
+		 * 1. x, x[.y[.z]...] -> property path, get value from given function, and get next from previous value, value must be a map. multiple segments are allowed,
+		 * 2. nextSeq -> next auto generated sequence,
+		 * 3. x.size, x[.y[.z]...].size -> property path, get value from given function, and size of this value. value must be a collection or a map,
+		 * 4. x.length, x[.y[.z]...].length -> property path, get value from given function, and size of this value. value must be a string.
+		 *
+		 * if any path returns a collection or an array, the next level will retrieve value from each elements in this collection or array.
+		 * IMPORTANT multiple collections or arrays are not supported yet.
+		 *
 		 * @param variable
-		 *  x, x[.y[.z]...] -> property path, get value from given function, and get next from previous value, value must be a map. multiple segments are allowed,
-		 *  nextSeq -> next auto generated sequence,
-		 *  x.size, x[.y[.z]...].size -> property path, get value from given function, and size of this value. value must be a collection or a map,
-		 *  x.length, x[.y[.z]...].length -> property path, get value from given function, and size of this value. value must be a string.
 		 * @param getFirstValue function to get value of first part of path. Ignored when it is {@code nextSeq}.
 		 * @param throws generate an exception string when path is incorrect.
 		 */
@@ -30,12 +35,12 @@ class ConstantParameterKits {
 					part == "size" && value is Collection<*> -> value.size
 					part == "size" && value is Map<*, *> -> value.size
 					part == "length" && value is String -> value.length
-					value is Map<*, Any?> -> value[part]
+					value is Map<*, *> -> value[part]
 					value is Collection<*> -> value.map {
-						if (it is Map<*, Any?>) it[part] else throw RuntimeException(throws())
+						if (it is Map<*, *>) it[part] else throw RuntimeException(throws())
 					}
 					value is Array<*> -> value.map {
-						if (it is Map<*, Any?>) it[part] else throw RuntimeException(throws())
+						if (it is Map<*, *>) it[part] else throw RuntimeException(throws())
 					}
 					else -> throw RuntimeException(throws())
 				}
