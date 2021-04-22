db9da558c2592cce8d0d899ff2719a1200c88f92
botlikes <bradwoo8621@yandex.com>
2021-04-22 17:38:24 +0800

    initialize

--- src/main/kotlin/com/imma/model/core/Enum.kt
+++ src/main/kotlin/com/imma/model/core/Enum.kt
@@ -7,30 +7,36 @@ import java.util.*
 
 @Entity
 data class EnumItem(
-    @Field("code")
-    var code: String? = null,
-    @Field("label")
-    var label: String? = null,
-    @Field("replace_code")
-    var replaceCode: String? = null,
-    @Field("parent_code")
-    var parentCode: String? = null
+    @Id("_id")
+    var enumItemId: String? = null,
+	@Field("code")
+	var code: String? = null,
+	@Field("label")
+	var label: String? = null,
+	@Field("replace_code")
+	var replaceCode: String? = null,
+	@Field("parent_code")
+	var parentCode: String? = null,
+	@CreatedAt("_create_time")
+	var createTime: Date? = null,
+	@LastModifiedAt("_last_modify_time")
+	var lastModifyTime: Date? = null,
 )
 
 @Entity(CollectionNames.ENUM)
 data class Enum(
-    @Id("_id")
-    var enumId: String? = null,
-    @Field("name")
-    var name: String? = null,
-    @Field("description")
-    var description: String? = null,
-    @Field("parent_enum_id")
-    var parentEnumId: String? = null,
-    @Transient
-    var items: MutableList<EnumItem> = mutableListOf(),
-    @CreatedAt("_create_time")
-    override var createTime: Date? = null,
-    @LastModifiedAt("_last_modify_time")
-    override var lastModifyTime: Date? = null,
+	@Id("_id")
+	var enumId: String? = null,
+	@Field("name")
+	var name: String? = null,
+	@Field("description")
+	var description: String? = null,
+	@Field("parent_enum_id")
+	var parentEnumId: String? = null,
+	@Transient
+	var items: MutableList<EnumItem> = mutableListOf(),
+	@CreatedAt("_create_time")
+	override var createTime: Date? = null,
+	@LastModifiedAt("_last_modify_time")
+	override var lastModifyTime: Date? = null,
 ) : Tuple()
--- src/main/kotlin/com/imma/service/core/log/PipelineLog.kt
+++ src/main/kotlin/com/imma/service/core/log/PipelineLog.kt
@@ -77,29 +77,3 @@ data class MonitorStageLog(
     val units: List<MonitorUnitLog>,
     val error: String?
 )
-
-@Entity(CollectionNames.PIPELINE_LOG)
-data class PipelineLog(
-    @Id("_id")
-    var uid: String,
-    @Field("pipeline_id")
-    var pipelineId: String,
-    @Field("topic_id")
-    var topicId: String,
-    @Field("status")
-    val status: RunStatus = RunStatus.done,
-    @Field("start_time")
-    val startTime: Date,
-    @Field("complete_time")
-    val completeTime: Double,
-    @Field("old_value")
-    val oldValue: Map<String, Any>,
-    @Field("new_value")
-    val newValue: Map<String, Any>,
-    @Field("condition_result")
-    val conditionResult: Boolean,
-    @Field("stages_log")
-    val stages: List<MonitorStageLog>,
-    @Field("error")
-    val error: String?
-)
