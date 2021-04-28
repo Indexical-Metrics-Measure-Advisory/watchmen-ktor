818f66ac7fe119446b5c4d17dba0ab18e79157fa
botlikes <bradwoo8621@yandex.com>
2021-04-28 10:37:52 +0800

    initialize

--- src/main/kotlin/com/imma/persist/oracle/OracleFunctions.kt
+++ src/main/kotlin/com/imma/persist/oracle/OracleFunctions.kt
@@ -4,94 +4,88 @@ import com.imma.persist.rdbms.RDBMSFunctions
 import com.imma.persist.rdbms.SQLPart
 
 class OracleFunctions : RDBMSFunctions() {
-	override fun year(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("YEAR(${one.statement})", one.values)
-	}
+    override fun year(one: SQLPart): SQLPart {
+        return SQLPart("YEAR(${one.statement})", one.values)
+    }
 
-	override fun halfYear(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("CEIL(QUARTER(${one.statement}) / 2)", one.values)
-	}
+    override fun halfYear(one: SQLPart): SQLPart {
+        return SQLPart("CASE WHEN MONTH(${one.statement}) <= 6 THEN 1 ELSE 2 END", one.values)
+    }
 
-	override fun quarter(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("QUARTER(${one.statement})", one.values)
-	}
+    override fun quarter(one: SQLPart): SQLPart {
+        return SQLPart("TO_NUMBER(TO_CHAR(${one.statement}, 'Q'))", one.values)
+    }
 
-	override fun month(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("MONTH(${one.statement})", one.values)
-	}
+    override fun month(one: SQLPart): SQLPart {
+        return SQLPart("MONTH(${one.statement})", one.values)
+    }
 
-	override fun weekOfYear(one: SQLPart): SQLPart {
-		TODO()
+    override fun weekOfYear(one: SQLPart): SQLPart {
+        TODO()
 //		return SQLPart("WEEK(${one.statement})", one.values)
-	}
+    }
 
-	override fun weekOfMonth(one: SQLPart): SQLPart {
-		TODO()
+    override fun weekOfMonth(one: SQLPart): SQLPart {
+        TODO()
 //		val values = mutableListOf<Any?>()
 //		repeat(5) { values.addAll(one.values) }
 //		return SQLPart(
 //			"IF(DAYOFWEEK(${one.statement}) <> 1, WEEK(${one.statement}) - WEEK(DATE_FORMAT(${one.statement}, '%Y-%m-01')), WEEK(${one.statement}) - WEEK(DATE_FORMAT(${one.statement}, '%Y-%m-01')) + 1)",
 //			values
 //		)
-	}
+    }
 
-	override fun dayOfMonth(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("DAYOFMONTH(${one.statement})", one.values)
-	}
+    override fun dayOfMonth(one: SQLPart): SQLPart {
+        return SQLPart("TO_NUMBER(TO_CHAR(${one.statement}), 'DD'))", one.values)
+    }
 
-	override fun dayOfWeek(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("DAYOFWEEK(${one.statement})", one.values)
-	}
+    override fun dayOfWeek(one: SQLPart): SQLPart {
+        return SQLPart("TO_NUMBER(TO_CHAR(${one.statement}, 'D'))", one.values)
+    }
 
-	override fun isEmpty(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("IF(${one.statement} IS NULL OR '', TRUE, FALSE)", one.values)
-	}
+    override fun isEmpty(one: SQLPart): SQLPart {
+        val values = mutableListOf<Any?>()
+        repeat(2) { values.addAll(one.values) }
+        return SQLPart("CASE WHEN ${one.statement} IS NULL OR ${one.statement} = '' THEN TRUE ELSE FALSE END", values)
+    }
 
-	override fun isNotEmpty(one: SQLPart): SQLPart {
-		TODO()
-//		return SQLPart("IF(${one.statement} IS NULL OR '', FALSE, TRUE)", one.values)
-	}
+    override fun isNotEmpty(one: SQLPart): SQLPart {
+        val values = mutableListOf<Any?>()
+        repeat(2) { values.addAll(one.values) }
+        return SQLPart("CASE WHEN ${one.statement} IS NULL OR ${one.statement} = '' THEN FALSE ELSE TRUE END", values)
+    }
 
-	/**
-	 * IMPORTANT only string value can be proceed correctly
-	 */
-	override fun hasOne(one: SQLPart, another: SQLPart): SQLPart {
-		TODO()
-//		val values = mutableListOf<Any?>()
-//		repeat(2) { values.addAll(one.values) }
-//		values.addAll(another.values)
-//		return SQLPart(
-//			"IF(${one.statement} IS NULL OR '', FALSE, IF(JSON_CONTAINS(${one.statement}, CAST(${another.statement} AS JSON)) = 0, FALSE, TRUE))",
-//			values
-//		)
-	}
+    /**
+     * IMPORTANT only string value can be proceed correctly
+     */
+    override fun hasOne(one: SQLPart, another: SQLPart): SQLPart {
+        val values = mutableListOf<Any?>()
+        repeat(3) { values.addAll(one.values) }
+        return SQLPart(
+            "CASE WHEN ${one.statement} IS NULL OR ${one.statement} = '' THEN FALSE WHEN JSON_EXISTS (${one.statement}, '\$[*]?(@ == \"${another.values[0]}\")') THEN TRUE ELSE FALSE END",
+            values
+        )
+    }
 
-	/**
-	 * IMPORTANT only string value can be proceed correctly
-	 */
-	override fun pull(fieldName: String, value: Any?): SQLPart {
-		TODO()
+    /**
+     * IMPORTANT only string value can be proceed correctly
+     */
+    override fun pull(fieldName: String, value: Any?): SQLPart {
+        TODO()
 //		return SQLPart(
 //			"$fieldName = IF($fieldName IS NULL OR '', $fieldName, JSON_REMOVE($fieldName, JSON_UNQUOTE(JSON_SEARCH($fieldName, 'one', ?))))",
 //			listOf(value, value)
 //		)
-	}
+    }
 
-	/**
-	 * IMPORTANT only string value can be proceed correctly
-	 */
-	override fun push(fieldName: String, value: Any?): SQLPart {
-		TODO()
+    /**
+     * IMPORTANT only string value can be proceed correctly
+     */
+    override fun push(fieldName: String, value: Any?): SQLPart {
+        TODO()
 //		return SQLPart(
 //			"$fieldName = IF($fieldName IS NULL OR '', CAST(CONCAT('[', ?, ']') AS JSON), JSON_ARRAY_APPEND($fieldName, '$', ?))",
 //			listOf(value, value)
 //		)
-	}
+    }
 }
