c42a959826e72ac8cea7a8390ccc7825f047a591
botlikes <bradwoo8621@yandex.com>
2021-07-29 03:52:15 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val jna_version: String by project
@@ -69,4 +71,13 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:$kotlin_version")
     implementation("net.java.dev.jna:jna:$jna_version")
 //    testImplementation("org.springframework.dataio.ktor:ktor-server-tests:$ktor_version")
+    implementation(kotlin("stdlib-jdk8"))
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
